37228e44cb8e13809815741e6176868bb24d2125
Kapilan Naidu <kapilan.naidu@me.com>
2025-10-14 13:53:33 +0800

    Templates Updated
    Implemented a more flexible template structure across p5-only and p5-arduino templates. Included helpers for both templates to handle window-sized sketch resizes, and Web Serial library functions.

--- starter_templates/p5-only/window-size/sketch.js
+++
@@ -1,9 +0,0 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
-
-function draw() {
-  background(100);
-  // min() uses the lower of two values
-  ellipse(width / 2, height / 2, min(width / 2, height / 2));
-}
